0c9e9c1f2f91820e132bf797420cce8973819056
Dominic Bisset <mail@dominicbisset.co.uk>
2016-06-14 21:49:51 +0100

    Updated package.json with repository info Added missing fields to player.ts

--- Scripts/Interfaces/Player.ts
+++ Scripts/Interfaces/Player.ts
@@ -1,24 +1,26 @@
 ﻿import {Character, Team, Season, PlaybookResult, Play, Tag} from "./Interfaces";
 
 export interface Player {
-        id: Number;
+    id: Number;
 
-        character: Character;
-        playsFor: Array<Team>;
-        validFor: Array<Season>;
+    character: Character;
+    playsFor: Array<Team>;
+    validFor: Array<Season>;
 
-        movJog: Number;
-        movRun: Number;
-        TAC: Number;
-        kickDice: Number;
-        kickDistance: Number;
-        def: Number;
-        arm: Number;
-        infGenerated: Number;
-        infMax: Number;
-        health: Number;
-        icySpongeLevels: Array<Number>;
-        playbook: Array<PlaybookResult>;
-        plays: Array<Play>;
-        tags: Array<Tag>;
+    movJog: Number;
+    movRun: Number;
+    TAC: Number;
+    kickDice: Number;
+    kickDistance: Number;
+    def: Number;
+    arm: Number;
+    infGenerated: Number;
+    infMax: Number;
+    health: Number;
+    baseSize: Number;
+    meleeZone: Number;
+    icySpongeLevels: Array<Number>;
+    playbook: Array<PlaybookResult>;
+    plays: Array<Play>;
+    tags: Array<Tag>;
 }
